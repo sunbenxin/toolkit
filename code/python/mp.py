0c6e60ef01b869b224e259280364b9eb16930b8b
sunbenxin <942171903@qq.com>
2016-09-26 15:10:55 +0800

    not store .pyc and add mp(multiprocess)

---
+++ code/python/mp.py
@@ -0,0 +1,21 @@
+from multiprocessing import Process
+
+
+def processfunc(*args):
+	print(args)
+
+
+if __name__ == "__main__":
+	pNum = 12
+	jobs = list()
+
+	for i in range(pNum):
+		j = Process(target=processfunc,args="args")
+
+		jobs.append(j)
+		j.start()
+
+	for j in jobs:
+		j.join()
+
+	
